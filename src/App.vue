4f01da8d59164e714bc8102d50a2b7927e30aa31
alirezaarv <alireza.arv.mail@gmail.com>
2019-04-20 18:05:59 +0430

    start animation

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,9 @@
 <template>
-  <section class="section">
-    <app-quote>
-      <h1>Heloooo</h1>
-      <p>Salam</p>
-    </app-quote>
-  </section>
+  <section class="section"></section>
 </template>
 
 <script>
-import Quote from "./components/Quote.vue";
-export default {
-  components: {
-    "app-quote": Quote
-  }
-};
+export default {};
 </script>
 
 <style lang="scss">
