ee82cb75093fb586f9a3bd8e3dead9ea3f0841e4
Melikabesharati <melika.besharati76@gmail.com>
2019-04-25 12:08:54 +0430

    bug fix

--- src/components/Header/GuestHeader.vue
+++ src/components/Header/GuestHeader.vue
@@ -1,24 +1,26 @@
 <template>
   <nav class="navbar is-info" role="navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item">
+        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+      </a>
+    </div>
+
     <div class="navbar-menu">
-      <div class="navbar-brand">
-        <a class="navbar-item">
-          <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-        </a>
-      </div>
-      
       <div class="navbar-start">
         <router-link tag="a" to="/" class="navbar-item">Home</router-link>
       </div>
 
       <div class="navbar-end">
-        <div class="buttons">
-          <router-link tag="a" to="/login" class="button is-dark">
-            <strong>Log in</strong>
-          </router-link>
-          <router-link tag="a" to="/register" class="button is-light">
-            <strong>Sign up</strong>
-          </router-link>
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-light">
+              <strong>Log in</strong>
+            </a>
+            <a class="button is-dark">
+              <strong>Sign up</strong>
+            </a>
+          </div>
         </div>
       </div>
     </div>
